4ed75692da4c2f2001295f8be088d6f0d1f422f5
ISLEM-NEDDAR <47813122+ISLEM-NEDDAR@users.noreply.github.com>
2019-04-11 17:37:45 +0200

    add the toolbar

--- app/src/main/java/com/example/nowdz/MainActivity.kt
+++ app/src/main/java/com/example/nowdz/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.nowdz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var topToolbar :Toolbar? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolbar()
+
+
+    }
+
+    /**
+     * Preparer le toolbar
+     */
+    private fun setToolbar(){
+        topToolbar = findViewById(R.id.toolbar_accuille)
+        setSupportActionBar(topToolbar)
     }
 }
