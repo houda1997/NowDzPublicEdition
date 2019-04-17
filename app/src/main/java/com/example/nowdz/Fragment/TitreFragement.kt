111ef0149dd0d49f835ec6e73ee1e221de550065
ISLEM-NEDDAR <fi_neddar@esi.dz>
2019-04-17 02:54:50 +0100

    add array categorie

--- app/src/main/java/com/example/nowdz/Fragment/TitreFragement.kt
+++ app/src/main/java/com/example/nowdz/Fragment/TitreFragement.kt
@@ -11,6 +11,9 @@ class TitreFragement : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_titre,null)
 
+
+
+
         return v
 
     }
