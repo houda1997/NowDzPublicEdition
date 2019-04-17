49021ff27937d7f981acad01bd0a27adc102600b
ISLEM-NEDDAR <fi_neddar@esi.dz>
2019-04-17 03:38:29 +0100

    add title page

--- app/src/main/java/com/example/nowdz/Fragment/TitreFragement.kt
+++ app/src/main/java/com/example/nowdz/Fragment/TitreFragement.kt
@@ -6,11 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nowdz.R
+import com.nshmura.recyclertablayout.RecyclerTabLayout
+import android.support.v4.view.ViewPager
+import com.example.nowdz.Adapter.TitlePagerAdapter
+
 
 class TitreFragement : Fragment() {
+    protected var mRecyclerTabLayout: RecyclerTabLayout? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_titre,null)
+        val listTitle = resources.getStringArray(R.array.type_news).toList()
 
+        val adapter = TitlePagerAdapter()
+        adapter.addAll(listTitle)
+
+        val viewPager = v.findViewById<ViewPager>(R.id.content_vp)
+        viewPager.adapter = adapter
+
+        mRecyclerTabLayout = v.findViewById(R.id.titre_tab_layout)
+        mRecyclerTabLayout!!.setUpWithViewPager(viewPager)
 
 
 
